7cdfa6b8595285812d995d8f50f69fb623ed5bbd
worldyone <amanohikari142857@gmail.com>
2020-06-26 17:41:37 +0900

    いくつかコマンド追加してテスト

--- discordbot.py
+++ discordbot.py
@@ -8,8 +8,11 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
 async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    """
+    コマンドエラー時メッセージ
+    """
+    # orig_error = getattr(error, "original", error)
+    # error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     # await ctx.send(error_msg)
     await ctx.send("そんなコマンド知らないにゃ！")
 
@@ -21,5 +24,51 @@ async def ping(ctx):
 @bot.command()
 async def neko(ctx):
     await ctx.send('nyan')
+
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send("result is " + (a + b))
+
+@bot.command()
+async def info(ctx):
+    embed = discord.Embed(title="nice bot", description="Nicest bot there is ever.", color=0xeee657)
+
+    # give info about you here
+    embed.add_field(name="Author", value="<YOUR-USERNAME>")
+
+    # Shows the number of servers the bot is member of.
+    embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
+
+    # give users a link to invite thsi bot to their server
+    embed.add_field(name="Invite", value="[Invite link](<insert your OAuth invitation link here>)")
+
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def refer(ctx):
+    """
+    振り返りのために、過去に対局した対局結果をランダムに配信してくれる
+
+    Parameters
+    ----------
+    ctx : ~ext.commands.Context
+        対象の果物のマスタID。
+    """
+    msg = "aa"
+    await ctx.send(msg)
+
+    member = Guild.get_member()
+    await ctx.send(member)
+
+
+def pred(m):
+    return m.author == message.author and m.channel == message.channel
+
+# try:
+#     msg = await client.wait_for('message', check=pred, timeout=60.0)
+# except asyncio.TimeoutError:
+#     await channel.send('You took too long...')
+# else:
+#     await channel.send('You said {0.content}, {0.author}.'.format(msg))
 
 bot.run(token)
