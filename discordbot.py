e1c113a892cca37699a769515bf8ee4f3dae6bf1
worldyone <amanohikari142857@gmail.com>
2020-06-26 16:33:57 +0900

    コミットテスト

--- discordbot.py
+++ discordbot.py
@@ -10,7 +10,8 @@ token = os.environ['DISCORD_BOT_TOKEN']
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+    # await ctx.send(error_msg)
+    await ctx.send("そんなコマンド知らないにゃ！")
 
 
 @bot.command()
