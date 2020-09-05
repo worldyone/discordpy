2416ef9a9fc406f876f27b9f92ca196aeb692047
worldyone <amanohikari142857@gmail.com>
2020-09-06 00:31:14 +0900

    クイズ機能の追加実装 単語帳作成

--- imoutocog.py
+++ imoutocog.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import inspect
 import random
 import wikipedia
+import requests
+from bs4 import BeautifulSoup as bs4
 
 
 class ImoutoCog(commands.Cog):
@@ -11,7 +13,10 @@ class ImoutoCog(commands.Cog):
     雑用兼テスト用
     """
 
+    wikipedia.set_lang("ja")
+
     wikipedia_page = wikipedia.page("wikipedia")
+    wordlist = []
 
     @commands.command()
     async def hello(self, ctx):
@@ -140,9 +145,10 @@ class ImoutoCog(commands.Cog):
 
     @quiz_wikipedia.command(aliases=['get', 'g'])
     async def get_random_wikipedia_page(self, ctx):
-        wikipedia.set_lang("ja")
+        """wikipediaからランダムな記事を一つ取得する"""
 
         await ctx.send("妹「wikipediaからランダムな記事を取ってくるね！」")
+        await ctx.send("妹は中空に手を翳し、何かを掴むような動作をしている。")
 
         # 日本語wikipediaからランダムな単語を一つ決めてページを取得する
         self.wikipedia_page = wikipedia.page(wikipedia.random())
@@ -158,9 +164,20 @@ class ImoutoCog(commands.Cog):
         hide_words.append(
             self.wikipedia_page.title.replace(" ", ""))  # 「霧雨 魔理沙」を「霧雨魔理沙」でもヒットするように
         # 「Python（パイソン）は、...」の「パイソン」を取得する
-        para_title = s[s.find("（") + 1: s.find("）")]
+        # 「ウォルト・ディズニー（Walt Disney, 1901年12月5日 - 1966年12月15日）...」とある場合、日付は削除したくないので句読点で避ける
+        punctuation_mark = [",", "、"]
+        start_parentheses = s.find("（")
+        end_parentheses = s.find("）")
+        end_para = end_parentheses
+        for mark in punctuation_mark:
+            mark_position = s.find(mark)
+            if start_parentheses < mark_position < end_parentheses:
+                end_para = mark_position
+        para_title = s[s.find("（") + 1: end_para]
+
         hide_words.append(para_title)
 
+        # **ANSWER**でマスクする
         for hide_word in hide_words:
             s = s.replace(hide_word, "**ANSWER**")
 
@@ -213,3 +230,52 @@ class ImoutoCog(commands.Cog):
             se2 = se2[s1: se2.find("\n", s1 + 1)]
             se2 = self.do_hide_words(se2)
             await ctx.send("妹「サマリーの二行目は\n「" + se2 + "\n」だよ！」")
+
+    @quiz_wikipedia.command(aliases=['cl', 'create_list'])
+    async def create_wordlist(self, ctx, target: str):
+        await ctx.send("妹「単語帳を作るよ！」")
+        await ctx.send("妹は懸命にペンを動かしている。")
+        if target == 'history':
+            self.wordlist = self.get_history_words()
+        if target == 'science':
+            self.wordlist = self.get_science_words()
+
+        await ctx.send("妹「単語帳を作成したよ！」")
+
+    def get_history_words(self):
+        """単語帳を作成する。
+
+        対象は'http://socialstudies.boy.jp/page-994/'にある歴史人物
+        """
+        url = 'http://socialstudies.boy.jp/page-994/'
+        page = requests.get(url)
+        soup = bs4(page.content, 'lxml')
+        tag_words = soup.find_all(class_='column-1')
+        words = [word.string for word in tag_words]
+
+        return words
+
+    def get_science_words(self):
+        """理化用の単語帳を作成する"""
+
+        url = 'http://rikamato.com/2017/12/15/science_word/'
+        page = requests.get(url)
+        soup = bs4(page.content, 'lxml')
+        tag_words = soup.find_all('tr')
+        words = [word.text.split("\n")[1] for word in tag_words[1:]]
+
+        return words
+
+    @quiz_wikipedia.command(aliases=['gil', 'get in list'])
+    async def get_wikipedia_page_for_wordlist(self, ctx):
+        """作成した単語帳からランダムで単語を選び、その単語でwikipediaのページを取得する"""
+
+        await ctx.send("妹「単語帳から適当に問題に出すね！」")
+        random_word = random.choice(self.wordlist)
+        self.wikipedia_page = wikipedia.page(random_word)
+        await ctx.send("妹は問題を書きとめ、あなたからの質問に応える気が十分なようだ")
+
+    @quiz_wikipedia.command(aliases=['d_show_wordlist'])
+    async def show_wordlist(self, ctx):
+        """デバッグ用。wordlistを見る"""
+        print(", ".join(self.wordlist))
