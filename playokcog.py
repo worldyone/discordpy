ea43a8e519a5bdbac3b325e6003acc1074216a23
worldyone <amanohikari142857@gmail.com>
2020-10-11 17:39:58 +0900

    Improve headlessで実行するように

--- playokcog.py
+++ playokcog.py
@@ -1,16 +1,19 @@
-from selenium import webdriver
 import time
+from selenium.webdriver import Chrome, ChromeOptions
+
+options = ChromeOptions()
+# ヘッドレスモードを有効にする（次の行をコメントアウトすると画面が表示される）。
+options.add_argument('--headless')
+# ChromeのWebDriverオブジェクトを作成する。
+driver = Chrome(options=options)
 
 user_id = "tako2"
 password = "xxxxxx"
 
-driver = webdriver.Chrome(
-    executable_path="D:/workspaces/discordpy-startup/libs/chromedriver_win32/chromedriver.exe")
-
-# # 1.Webサイトにアクセスする
+# 1.Webサイトにアクセスする
 driver.get('https://www.playok.com/ja/go/')
 
-# 3. ログイン情報を入力してログインする
+# 2. ログイン情報を入力してログインする
 tag = driver.find_element_by_xpath(
     '/html/body/div[2]/div[1]/div/table/tbody/tr/td[1]/button')
 tag.click()
@@ -21,18 +24,19 @@ p.send_keys(password)
 p.submit()
 time.sleep(1)
 
+# 3. 「スタート - 碁」ボタンを押下する
 tag = driver.find_element_by_xpath('//*[@id="bast"]/p[1]/button')
 tag.click()
 
 handle_array = driver.window_handles
 driver.switch_to.window(handle_array[-1])
 time.sleep(3)
-print(driver.current_url)
 
-# 6. 「テーブル作成」ボタンを押下する
-# tag = driver.find_element_by_xpath(
-# '//*[@id="appcont"]/div[1]/div[1]/div[1]/button')
+# 4. 「テーブル作成」ボタンを押下する
 tag = driver.find_element_by_xpath(
     '/html/body/div/div[1]/div[1]/div[1]/button')
 tag.click()
 time.sleep(1)
+
+# 5. 現在のURLを出力
+print(driver.current_url)
