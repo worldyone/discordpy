2294be471b3e2ab2b91030b73f90d19e313b384e
worldyone <amanohikari142857@gmail.com>
2020-10-11 18:39:56 +0900

    plyaokcogをボット化

--- playokcog.py
+++ playokcog.py
@@ -1,42 +1,83 @@
+import discord
+from discord.ext import commands
 import time
 from selenium.webdriver import Chrome, ChromeOptions
 
-options = ChromeOptions()
-# ヘッドレスモードを有効にする（次の行をコメントアウトすると画面が表示される）。
-options.add_argument('--headless')
-# ChromeのWebDriverオブジェクトを作成する。
-driver = Chrome(options=options)
 
-user_id = "tako2"
-password = "xxxxxx"
+class PlayokCog(commands.Cog):
 
-# 1.Webサイトにアクセスする
-driver.get('https://www.playok.com/ja/go/')
+    USER_ID = "tako2"
+    PASSWORD = "xxxxxx"
 
-# 2. ログイン情報を入力してログインする
-tag = driver.find_element_by_xpath(
-    '/html/body/div[2]/div[1]/div/table/tbody/tr/td[1]/button')
-tag.click()
-u = driver.find_element_by_name('username')
-u.send_keys(user_id)
-p = driver.find_element_by_name('pw')
-p.send_keys(password)
-p.submit()
-time.sleep(1)
+    @commands.group(aliases=['playok', 'po', 'ok'])
+    async def playOK(self, ctx):
+        if ctx.invoked_subcommand is None:
+            embed = discord.Embed(
+                title="playOKについて",
+                description="playOKについて"
+            )
 
-# 3. 「スタート - 碁」ボタンを押下する
-tag = driver.find_element_by_xpath('//*[@id="bast"]/p[1]/button')
-tag.click()
+            embed.add_field(
+                name="create_room", value="部屋の作成\n        e.ok cr 将棋", inline=False)
 
-handle_array = driver.window_handles
-driver.switch_to.window(handle_array[-1])
-time.sleep(3)
+            await ctx.send(embed=embed)
 
-# 4. 「テーブル作成」ボタンを押下する
-tag = driver.find_element_by_xpath(
-    '/html/body/div/div[1]/div[1]/div[1]/button')
-tag.click()
-time.sleep(1)
+    @playOK.command(aliases=['create_room', 'cr'])
+    async def create_playing_room(self, ctx, event: str):
+        """
+        PlayOKに自動ログインして、テーブル作成を実施して、部屋のURLを返す機能
 
-# 5. 現在のURLを出力
-print(driver.current_url)
+        ログインID, ログインパスワードは、作成者:worldyのものを使用する。
+        """
+
+        options = ChromeOptions()
+        # ヘッドレスモードを有効にする（次の行をコメントアウトすると画面が表示される）。
+        options.add_argument('--headless')
+        # ChromeのWebDriverオブジェクトを作成する。
+        driver = Chrome(options=options)
+
+        # Webサイトにアクセスする
+        if event in ["碁", "囲碁", "go"]:
+            await ctx.send("妹「囲碁の部屋を作成するね！」")
+            path = 'https://www.playok.com/ja/go/'
+        elif event in ["将棋", "shogi"]:
+            await ctx.send("妹「将棋の部屋を作成するね！」")
+            path = 'https://www.playok.com/ja/shogi/'
+        elif event in ["オセロ", "リバーシ", "othello", "reversi"]:
+            await ctx.send("妹「オセロの部屋を作成するね！」")
+            path = 'https://www.playok.com/ja/reversi/'
+        else:
+            # 「囲碁・将棋・オセロ」の中から選んでね！」
+            await ctx.send("妹「囲碁・将棋・オセロ」の中から選んでね！」")
+            return
+
+        # driverの取得
+        driver.get(path)
+
+        # ログイン情報を入力してログインする
+        tag = driver.find_element_by_xpath(
+            '/html/body/div[2]/div[1]/div/table/tbody/tr/td[1]/button')
+        tag.click()
+        u = driver.find_element_by_name('username')
+        u.send_keys(self.USER_ID)
+        p = driver.find_element_by_name('pw')
+        p.send_keys(self.PASSWORD)
+        p.submit()
+        time.sleep(3)
+
+        # 「スタート - {イベント名}」ボタンを押下する
+        tag = driver.find_element_by_xpath('//*[@id="bast"]/p[1]/button')
+        tag.click()
+
+        handle_array = driver.window_handles
+        driver.switch_to.window(handle_array[-1])
+        time.sleep(5)
+
+        # 「テーブル作成」ボタンを押下する
+        tag = driver.find_element_by_xpath(
+            '/html/body/div/div[1]/div[1]/div[1]/button')
+        tag.click()
+        time.sleep(3)
+
+        # 作成した部屋のURLを返す
+        await ctx.send(driver.current_url)
